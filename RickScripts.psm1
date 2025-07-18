720ede0ada25c94816f17a27b0f6982fcd8fbb42
Ricardo Diaz <rick.diaz@world50.com>
2025-07-18 10:55:53 -0400

    fix:Removed global variables

--- RickScripts.psm1
+++ RickScripts.psm1
@@ -1,7 +1,7 @@
 # Dot-source all function files
 $FunctionFiles = Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1
 foreach ($File in $FunctionFiles) {
-    . $File.FullName
+	. $File.FullName
 }
 
 # Export functions
@@ -9,10 +9,3 @@ Export-ModuleMember -Function Switch-Branch, Switch-MergeRequest, Remove-LocalBr
 
 # Export aliases
 Export-ModuleMember -Alias sb, smr, rlb, cnm, opf, nmr, ggs, rhi, rhd, mis
-
-# Export global variables for package filters
-$global:mem = "--filter=@world50/member-app"
-$global:gl = "--filter=@world50/group-leader-app"
-$global:pro = "--filter=@world50/prospector-app"
-$global:auth = "--filter=@world50/authentication-app"
-$global:petl = "--filter=@world50/prospector-etl"
