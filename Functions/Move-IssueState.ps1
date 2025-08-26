24e118d8f574e78a32842d411000363f78fead9a
Ricardo Diaz <rick.diaz@world50.com>
2025-08-26 11:01:00 -0400

    fix:Fix glab command syntax for label operations

--- Functions/Move-IssueState.ps1
+++ Functions/Move-IssueState.ps1
@@ -72,36 +72,36 @@ function Move-IssueState {
     $existingNonStateLabels = $currentLabels | Where-Object { $_ -notin $allStateLabels -and $_ -notin $labelsToRemove }
     $allLabelsToSet = $existingNonStateLabels + $labelsToAdd
 
-    function Add-LabelUpdates {
-        param($args, $currentStateLabel, $labelsToRemove, $allLabelsToSet)
+    function Update-IssueLabels {
+        param($issueNumber, $currentStateLabel, $labelsToRemove, $labelsToAdd)
         
+        # Remove labels (including current state label)
+        $allLabelsToRemove = @()
         if ($currentStateLabel) {
-            $args += "--unlabel"
-            $args += $currentStateLabel
+            $allLabelsToRemove += $currentStateLabel
+        }
+        $allLabelsToRemove += $labelsToRemove
+        
+        if ($allLabelsToRemove.Count -gt 0) {
+            $removeLabelsString = $allLabelsToRemove -join ","
+            $removeArgs = @("issue", "update", $issueNumber, "--unlabel", $removeLabelsString)
+            $removeCommandString = "glab " + ($removeArgs -join " ")
+            Write-Debug "Remove labels command: $removeCommandString"
+            & glab @removeArgs
         }
         
-        foreach ($labelToRemove in $labelsToRemove) {
-            $args += "--unlabel"
-            $args += $labelToRemove
+        # Add labels
+        if ($labelsToAdd.Count -gt 0) {
+            $addLabelsString = $labelsToAdd -join ","
+            $addArgs = @("issue", "update", $issueNumber, "--label", $addLabelsString)
+            $addCommandString = "glab " + ($addArgs -join " ")
+            Write-Debug "Add labels command: $addCommandString"
+            & glab @addArgs
         }
-        
-        foreach ($label in $allLabelsToSet) {
-            $args += "--label"
-            $args += $label
-        }
-        
-        return $args
     }
     
-    $glabArgs = @("issue", "update", $IssueNumber)
-    $glabArgs = Add-LabelUpdates $glabArgs $currentStateLabel $labelsToRemove $allLabelsToSet
-    
-    $commandString = "glab " + ($glabArgs -join " ")
-    
-    Write-Debug "Command to execute: $commandString"
-    
-    if ($PSCmdlet.ShouldProcess("Issue #$IssueNumber", "Execute: $commandString")) {
-        & glab @glabArgs
+    if ($PSCmdlet.ShouldProcess("Issue #$IssueNumber", "Update labels")) {
+        Update-IssueLabels $IssueNumber $currentStateLabel $labelsToRemove $labelsToAdd
         
         # Find and update associated MR with the same labels
         try {
@@ -111,13 +111,36 @@ function Move-IssueState {
             if ($relatedMR) {
                 Write-Debug "Found related MR: !$($relatedMR.iid) - $($relatedMR.title)"
                 
-                $mrGlabArgs = @("mr", "update", $relatedMR.iid)
-                $mrGlabArgs = Add-LabelUpdates $mrGlabArgs $currentStateLabel $labelsToRemove $allLabelsToSet
+                # Update MR labels using the same approach as issues
+                function Update-MRLabels {
+                    param($mrNumber, $currentStateLabel, $labelsToRemove, $labelsToAdd)
+                    
+                    # Remove labels (including current state label)
+                    $allLabelsToRemove = @()
+                    if ($currentStateLabel) {
+                        $allLabelsToRemove += $currentStateLabel
+                    }
+                    $allLabelsToRemove += $labelsToRemove
+                    
+                    if ($allLabelsToRemove.Count -gt 0) {
+                        $removeLabelsString = $allLabelsToRemove -join ","
+                        $removeArgs = @("mr", "update", $mrNumber, "--unlabel", $removeLabelsString)
+                        $removeCommandString = "glab " + ($removeArgs -join " ")
+                        Write-Debug "MR Remove labels command: $removeCommandString"
+                        & glab @removeArgs
+                    }
+                    
+                    # Add labels
+                    if ($labelsToAdd.Count -gt 0) {
+                        $addLabelsString = $labelsToAdd -join ","
+                        $addArgs = @("mr", "update", $mrNumber, "--label", $addLabelsString)
+                        $addCommandString = "glab " + ($addArgs -join " ")
+                        Write-Debug "MR Add labels command: $addCommandString"
+                        & glab @addArgs
+                    }
+                }
                 
-                $mrCommandString = "glab " + ($mrGlabArgs -join " ")
-                Write-Debug "MR Command to execute: $mrCommandString"
-                
-                & glab @mrGlabArgs
+                Update-MRLabels $relatedMR.iid $currentStateLabel $labelsToRemove $labelsToAdd
             }
             else {
                 Write-Debug "No related MR found for issue #$IssueNumber"
@@ -130,5 +153,6 @@ function Move-IssueState {
 }
 
 Set-Alias mis Move-IssueState
+
 
 
