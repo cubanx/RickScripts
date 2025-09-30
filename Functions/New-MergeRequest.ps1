22f301947b3d553b227e616bddd5b12e019dbbf7
Ricardo Diaz <rick.diaz@world50.com>
2025-09-30 14:23:50 -0400

    feat:Limit branch names to not hurt Okteto's brain

--- Functions/New-MergeRequest.ps1
+++ Functions/New-MergeRequest.ps1
@@ -50,6 +50,47 @@ function New-MergeRequest {
     }
     $FormattedTitle = "[#${IssueNumber}] $Title"
 
+    $branchSlug = ($Title.ToLower() -replace '[^a-z0-9]+', '-')
+    $branchSlug = $branchSlug.Trim('-')
+    $issuePrefix = $IssueNumber.ToString()
+    $branchName = if ($branchSlug) { "$issuePrefix-$branchSlug" } else { $issuePrefix }
+
+    $MaxBranchLength = 55
+    if ($branchName.Length -gt $MaxBranchLength) {
+        $remainingLength = $MaxBranchLength - ($issuePrefix.Length + 1)
+        $suggestions = @()
+
+        if ($remainingLength -gt 0) {
+            $truncatedSlug = $branchSlug.Substring(0, [Math]::Min($remainingLength, $branchSlug.Length))
+            $truncatedSlug = $truncatedSlug.Trim('-')
+            if ($truncatedSlug) {
+                $suggestions += "$issuePrefix-$truncatedSlug"
+            }
+        }
+
+        $words = $branchSlug.Split('-', [System.StringSplitOptions]::RemoveEmptyEntries)
+        if ($words.Count -gt 1) {
+            $initialsSlug = ($words | ForEach-Object { $_[0] }) -join ''
+            if ($initialsSlug) {
+                $initialBranch = "$issuePrefix-$initialsSlug"
+                if ($initialBranch.Length -le $MaxBranchLength) {
+                    $suggestions += $initialBranch
+                }
+            }
+        }
+
+        if (-not $suggestions) {
+            $suggestions += $issuePrefix
+        }
+
+        Write-Error (@(
+                "Cannot create merge request because the inferred branch name '$branchName' would be $($branchName.Length) characters (max $MaxBranchLength).",
+                "Suggested shorter branch names:",
+                ($suggestions | ForEach-Object { "  - $_" })
+            ) -join [Environment]::NewLine)
+        return
+    }
+
     glab issue update $IssueNumber --assignee rick.diaz
 
     $glabArgs = @(
@@ -76,6 +117,7 @@ function New-MergeRequest {
 }
 
 Set-Alias nmr New-MergeRequest
+
 
 
 
